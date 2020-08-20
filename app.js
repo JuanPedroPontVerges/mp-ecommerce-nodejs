1ee46e58b2bed5e49f69d6b2fa041a6032e51fa8
PontVergesJuanPedro <110959@tecnicatura.frc.utn.edu.ar>
2020-08-20 18:27:02 -0300

    a

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+const port = 3000 || process.env.PORT
  
 var app = express();
  
@@ -18,4 +19,4 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
-app.listen(3000);
+app.listen(port);
